60e2f7731dfb21b76a0cf6526537244f7bd0fdda
Bogdan Nasypanyi <nasipanijbogdan@gmail.com>
2024-09-17 11:13:11 +0300

    done

---
+++ holiday.py
@@ -0,0 +1,27 @@
+# Лабораторні роботи з дисципліни "Алгоритмізація та програмування"
+# Виконав:Насипаний Богдан Вікторович(ІР-14)
+# Лабораторна робота №1 (Варіант 15)
+# Створіть змінну `holiday` та присвойте їй назву вашого улюбленого свята. 
+# Виведіть рядок, який містить назву свята.
+
+holiday = "birthday"
+print( holiday )
+is_adult = True
+is_teen = False
+
+print(is_adult and is_teen)
+#print()
+#print()
+
+is_windy = True
+is_calm = True
+result = is_windy or is_calm
+print(result)
+
+import math
+
+x = 0.013
+z = 1.245
+result = math.sin(x ** math.cos(z)) + math.tan(z) ** math.sin(x)
+print(result)
+
